6de53bee6ce3eb15952364b965bdd3829650bda4
Serg8787 <tsybulnikserg@gmail.com>
2021-07-16 14:07:11 +0300

    without disain

--- app/src/main/java/com/example/carddogs/FragmentDetail.kt
+++ app/src/main/java/com/example/carddogs/FragmentDetail.kt
@@ -7,12 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.item_dog.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-private const val ARG_PARAM3 = "param3"
 
 /**
  * A simple [Fragment] subclass.
@@ -20,9 +16,8 @@ private const val ARG_PARAM3 = "param3"
  * create an instance of this fragment.
  */
 class FragmentDetail : Fragment() {
-
     private var name: String? = null
-    private var lastName: String? = null
+    private var specias: String? = null
     private var desc: String? = null
     private var allDesc: String? = null
     private var fact: String? = null
@@ -30,12 +25,6 @@ class FragmentDetail : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            name = it.getString(ARG_PARAM1)
-            lastName = it.getString(ARG_PARAM2)
-            desc = it.getString(ARG_PARAM3)
-//            avatar = it.getString(ARG_PARAM3)
-        }
     }
 
     override fun onCreateView(
@@ -49,8 +38,9 @@ class FragmentDetail : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tvNameDetail.text = name
-        tvDescDetail.text = desc
+        tvSpeciasDetail.text = specias
         tvAllDescDetail.text = allDesc
+        tvDescDetail.text = desc
         ivAvatarDetail.setImageBitmap(avatarImage)
         tvFactDetail.text = fact
     }
@@ -59,17 +49,11 @@ class FragmentDetail : Fragment() {
         @JvmStatic
         fun newInstance(dog: Dog): FragmentDetail =
             FragmentDetail().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, dog.name)
-//                    putString(ARG_PARAM2, dog.lastName)
-                    putString(ARG_PARAM3, dog.desc)
-
-                }
                 desc = dog.desc
                 allDesc = dog.allDesc
                 avatarImage = dog.avatar
                 fact = dog.facts
-
+                specias = dog.specias
             }
     }
 }
